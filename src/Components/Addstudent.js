5420f8b03ce2b55562277ae637f3979f1c0357a5
ibrahim musa <miharbiibra675@gmail.com>
2024-11-05 12:16:04 +0300

    addstudent.js

--- src/Components/Addstudent.js
+++ src/Components/Addstudent.js
@@ -1,27 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-const Addstudent = () => {
+import { useState } from "react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+
+const AddStudent = () => {
+  const [data, setData] = useState({
+    firstname: "",
+    lastname: "",
+    gender: ""
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const saveStudent = (e) => {
+    e.preventDefault();
+    const token = sessionStorage.getItem("access_token");
+
+    axios
+      .post('http://localhost:4000/addstudent', data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(res => {
+        toast.success("Student added successfully!", {
+         
+        });
+      })
+      .catch((error) => {
+        toast.error("Failed to add student. Please try again.", {
+          
+        });
+      });
+  };
+
   return (
     <div className="form-container">
-     
-      <form>
+      <h2>Add New Student</h2>
+      <form onSubmit={saveStudent}>
         <div>
-          <label>Email</label>
-          <input type="email" placeholder="Enter email" />
+          <label>Firstname:</label>
+          <input type="text" name="firstname" placeholder="Enter first name"  onChange={handleChange} required />
         </div>
-        
         <div>
-          <label>Password</label>
-          <input type="password" placeholder="Enter password" />
+          <label>Lastname:</label>
+          <input type="text"  name="lastname" placeholder="Enter last name" onChange={handleChange} required />
         </div>
-        
-        <p>Don't have an account <Link to="/register" className="register-link">Register?</Link></p>
-
-
-        <button type="submit">Submit</button>
+        <div>
+          <label>Gender:</label>
+          <input type="text" name="gender" placeholder="Enter gender"  onChange={handleChange} required />
+        </div>
+        <button type="submit">Add Student</button>
       </form>
+      <ToastContainer />
     </div>
   );
 };
 
-export default Addstudent;
+export default AddStudent;
