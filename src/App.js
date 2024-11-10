b690be7c21bb420178199a73e3b120fbd81de505
ibrahim musa <miharbiibra675@gmail.com>
2024-11-10 15:38:56 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-// import Register from './Components/Register';
-
 import AddStudent from './Components/AddStudent';
-
+import AllStudents from './Components/AllStudents';
+import UpdateStudent from './Components/UpdateStudent';
 
 function App() {
   return (
     <Router>
       <div className="App">
-    
         <Navbar />
         <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/add-student" element={<AddStudent />} />
-            {/* <Route path="/register" element={<Register />} /> */}
-          </Routes>
+          <Switch> 
+            <Route path="/all-students" component={AllStudents} />
+            <Route path="/add-student" component={AddStudent} />
+            <Route path="/updatestudent/:id" component={UpdateStudent} />
+          </Switch>
         </div>
       </div>
     </Router>
@@ -26,5 +23,3 @@ function App() {
 }
 
 export default App;
-
-
