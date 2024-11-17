30f503a94578c2ece080c25290d51a402321c10a
ibrahim musa <miharbiibra675@gmail.com>
2024-11-17 14:04:06 +0300

    LogOut.js

---
+++ src/Components/LogOut.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { AuthContext } from "./Auth";
+import { useState, useContext } from 'react';
+import { useHistory } from "react-router-dom";
+
+const LogOut = () => {
+  const { logout } = useContext(AuthContext);
+  const navigate = useHistory();
+
+  const handleLogout = async () => {
+
+    // try {
+    //   await logout(); // This will trigger both client-side and server-side logout
+    // } catch (error) {
+    //   console.error(
+    //     "Error during logout:",
+    //     error.response?.data.error || error.message
+    //   );
+    //   // Handle error scenario, e.g., display an error message to the user
+    // }
+    logout(navigate);
+  };
+
+  
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.content}>
+        <p>Are you sure you want to log out?</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh", // Adjust as needed
+  },
+  content: {
+    textAlign: "center",
+  },
+};
+
+export default LogOut;
